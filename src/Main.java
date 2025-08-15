bf735ce24e9993921dc8ea24e37fdd3274fe8b71
yash <yssh88972@gmail.com>
2025-08-15 12:34:27 +0530

    add swing gui

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,43 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    JFrame frame = new JFrame();
+
+    ImageIcon image = new ImageIcon("C:\\Users\\yashs\\OneDrive\\Desktop\\Projects\\Calculator\\src\\calculator.png");
 
 
-    double num1, num2;
-    System.out.println("Enter number 1:");
-    num1 = scanner.nextDouble();
-    System.out.println("Enter number 2 :");
-    num2 = scanner.nextDouble();
+    int height = 550;
+    int width = 400;
 
+    frame.setSize(width,height);
+    frame.setTitle("Calculator");
+    frame.setResizable(false);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+    frame.setIconImage(image.getImage());
+    frame.getContentPane().setBackground(new Color(59, 59, 59));
 
-
-    if(num1 == 0 ||num2 == 0){
-        System.out.println("Cannot divide by Zero");
-    }
-    else {
-        System.out.println(add(num1, num2));
-        System.out.println(sub(num1, num2));
-        System.out.println(mul(num1, num2));
-        System.out.println(div(num1, num2));
-    }
+//    double num1, num2;
+//    System.out.println("Enter number 1:");
+//    num1 = scanner.nextDouble();
+//    System.out.println("Enter number 2 :");
+//    num2 = scanner.nextDouble();
+//
+//
+//
+//    if(num1 == 0 ||num2 == 0){
+//        System.out.println("Cannot divide by Zero");
+//    }
+//    else {
+//        System.out.println(add(num1, num2));
+//        System.out.println(sub(num1, num2));
+//        System.out.println(mul(num1, num2));
+//        System.out.println(div(num1, num2));
+//    }
 
         scanner.close();
     }
